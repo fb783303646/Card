ae0ec46caf464f02aa95e12bef99653691039af4
fb <fb@VK>
2017-07-14 16:51:44 +0800

    修改选择框数据

--- src/app/pages/personal/personal.component.ts
+++ src/app/pages/personal/personal.component.ts
@@ -10,14 +10,26 @@ export class PersonalComponent implements OnInit {
 
   Packages;
   isNewCreate = false;
-  cities5 = [];
-  selectedCity5: any;
+  packageArry = [];
+  cycleArry = [];
+  operatorArry = [];
+  packageSelects: any;
+  cycleSelect: any;
+  operatorSelect: any;
 
   constructor(private Carservice: CarService ) { }
 
   ngOnInit() {
     this.getPackgages();
-    this.cities5 = [
+    this.packageArry = [
+      { label: '10M/月', value: { id: 2, name: '10M/月', code: 'RM' } },
+      { label: '100M/月', value: { id: 3, name: '100M/月', code: 'LDN' } },
+    ];
+    this.cycleArry = [
+      { label: '10M/月', value: { id: 2, name: '10M/月', code: 'RM' } },
+      { label: '100M/月', value: { id: 3, name: '100M/月', code: 'LDN' } },
+    ];
+    this.operatorArry = [
       { label: '10M/月', value: { id: 2, name: '10M/月', code: 'RM' } },
       { label: '100M/月', value: { id: 3, name: '100M/月', code: 'LDN' } },
     ];
